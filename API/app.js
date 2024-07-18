c0c28f1c3f36d438125dc751089344e844ba5a62
Mufaddal Saifee <mufaddalsaifee2812@gmail.com>
2024-07-18 18:47:11 +0530

    Adding project

---
+++ API/app.js
@@ -0,0 +1,39 @@
+// Requiring the packages I installed
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const User = require("./model/userSchema");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// I am using the dotenv to hide my database URL and PORT number
+dotenv.config({ path: "./config.env" });
+// const DB =
+const DB = process.env.DATABASE;
+const port = process.env.PORT;
+
+// connecting to the Data Base using Mongoose
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to the Data Base successfully");
+  })
+  .catch((Error) => {
+    console.log(Error, "This error occured while connecting Data Base");
+  });
+
+app.use(express.json());
+
+// Middle ware for Login Authentication
+app.use(require("./router/auth"));
+
+// Server Listning PORT
+app.listen(port, () => {
+  console.log(`Your server is running on port http://localhost:${port}`);
+});
